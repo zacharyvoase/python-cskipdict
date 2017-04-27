44fb1217185d7a041c0562c4486b5a11f60e074e
Zachary Voase <zack@meat.io>
2017-04-27 14:46:22 -0700

    Bump to v0.0.2.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='cskipdict',
-    version='0.0.1',
+    version='0.0.2',
 
     description='A fast CFFI-based SkipDict implementation.',
     long_description=readme,
